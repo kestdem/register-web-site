01de8f23a652afde318c69ce20f96a6bbba9573b
plain mind <83367482+kestdem@users.noreply.github.com>
2024-12-14 19:34:10 +0300

    Add files via upload

---
+++ js/connect.js
@@ -0,0 +1,7 @@
+window.studentRegister = function() {
+    window.location.href = "panels/student-register-panel.html";
+};
+
+window.userLogin = function() {
+    window.location.href = "panels/login-panel.html";
+};
