c303375947b6efe88d38041284960c9047b7991d
plain mind <83367482+kestdem@users.noreply.github.com>
2024-12-20 17:24:58 +0300

    Delete js directory
    delete all

--- js/connect.js
+++
@@ -1,7 +0,0 @@
-window.studentRegister = function() {
-    window.location.href = "panels/student-register-panel.html";
-};
-
-window.userLogin = function() {
-    window.location.href = "panels/login-panel.html";
-};
